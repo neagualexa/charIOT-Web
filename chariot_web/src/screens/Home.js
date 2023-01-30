174e9f3197340fdddfb3120b185c3e5c32d7f64e
ngu_alexandra <ngu.alexandra@gmail.com>
2023-01-30 15:53:01 +0000

    graph library added yay

--- chariot_web/src/screens/Home.js
+++ chariot_web/src/screens/Home.js
@@ -9,11 +9,13 @@ import "@aws-amplify/ui-react/styles.css";
 // } from "@aws-amplify/ui-react";
 import React from 'react';
 import '../App.css';
+import BarGraph from "../components/chart";
 
 function Home() {
   return (
     <div className="App">
       <div className="App-text">HOME</div>
+      <BarGraph className="App"/>
     </div>
   );
 }
