cedc22b06df485e85ab559bcef8d0f7563d54c52
ngu_alexandra <ngu.alexandra@gmail.com>
2023-02-15 13:16:26 +0000

    api key updated

--- chariot_web/src/aws-exports.js
+++ chariot_web/src/aws-exports.js
@@ -27,7 +27,7 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://wtfcdfkb7nfsfaow3srahimkxy.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-4mbw43tzjvcj5oymmy52olfpv4"
+    "aws_appsync_apiKey": "da2-dq2tcd4whbe4zkxto6qnifmdui"
 };
 
 
